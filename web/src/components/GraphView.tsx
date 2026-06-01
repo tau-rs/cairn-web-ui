55f8c4f8682069b6d718eacc0759bb19ba876bf9
titouanlebocq <lebocq.titouan@gmail.com>
2026-06-01 18:35:17 +0200

    feat: GraphView React Flow wrapper

---
+++ web/src/components/GraphView.tsx
@@ -0,0 +1,37 @@
+import { useMemo } from "react";
+import {
+  ReactFlow,
+  Background,
+  Controls,
+  type Node,
+  type Edge,
+} from "@xyflow/react";
+import { computeGraphLayout } from "./graph/computeLayout";
+import { buildFlowElements } from "./graph/buildFlowElements";
+
+export function GraphView(props: {
+  nodes: string[];
+  edges: { from: string; to: string }[];
+  activePath: string | null;
+  onOpenNote: (path: string) => void;
+}) {
+  const elements = useMemo(() => {
+    const positions = computeGraphLayout(props.nodes, props.edges);
+    return buildFlowElements(props.nodes, props.edges, positions, props.activePath);
+  }, [props.nodes, props.edges, props.activePath]);
+
+  return (
+    <div className="h-full w-full">
+      <ReactFlow
+        nodes={elements.nodes as Node[]}
+        edges={elements.edges as Edge[]}
+        fitView
+        proOptions={{ hideAttribution: true }}
+        onNodeClick={(_, node) => props.onOpenNote(node.id)}
+      >
+        <Background />
+        <Controls />
+      </ReactFlow>
+    </div>
+  );
+}
