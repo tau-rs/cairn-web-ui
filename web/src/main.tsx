a76ae14b75346668e0fed36f5d652edaba6dfc72
titouanlebocq <lebocq.titouan@gmail.com>
2026-06-01 18:29:18 +0200

    build: add @xyflow/react + d3-force for the graph view

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./app/App";
 import "./index.css";
+import "@xyflow/react/dist/style.css";
 import "highlight.js/styles/github-dark.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
