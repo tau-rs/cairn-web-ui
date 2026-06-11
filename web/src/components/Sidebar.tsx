c98725062c72b0262352aafdb6ab76ef61f8cc5d
titouanlebocq <lebocq.titouan@gmail.com>
2026-06-11 15:49:42 +0200

    refactor(app): extract Sidebar component

---
+++ web/src/components/Sidebar.tsx
@@ -0,0 +1,37 @@
+import { useNavigate } from "react-router-dom";
+import { useCairn, cairnStore } from "../app/cairnStore";
+import { noteUrl, tagUrl } from "../app/routes";
+import { FolderTree } from "./tree/FolderTreeView";
+import { TagsPanel } from "./tags/TagsPanel";
+
+export function Sidebar() {
+  const navigate = useNavigate();
+  const actions = cairnStore.getState();
+  const notePaths = useCairn((s) => s.notePaths);
+  const activePath = useCairn((s) => s.activePath);
+  const tags = useCairn((s) => s.tags);
+  const activeTag = useCairn((s) => s.activeTag);
+
+  return (
+    <>
+      <FolderTree
+        paths={notePaths}
+        activePath={activePath}
+        onOpen={(p) => navigate(noteUrl(p))}
+        onDelete={actions.deleteNote}
+        onRequestNew={() =>
+          actions.setUi({ newNoteInitial: "", newNoteOpen: true })
+        }
+        onRequestNewInFolder={(folder) =>
+          actions.setUi({ newNoteInitial: folder + "/", newNoteOpen: true })
+        }
+        onApplyRenames={actions.applyRenames}
+      />
+      <TagsPanel
+        tags={tags}
+        activeTag={activeTag}
+        onSelect={(t) => navigate(tagUrl(t))}
+      />
+    </>
+  );
+}
